0edd34730b947cf448d5d0d63a4385865a547a89
Andy Tuddenham <andy@tuddenham.org>
2022-07-25 11:39:42 +0100

    Resolving issue #1: Custom properties file is not loaded

--- src/com/tudders/dolphin/times/Main.java
+++ src/com/tudders/dolphin/times/Main.java
@@ -5,18 +5,11 @@ import javax.swing.SwingUtilities;
 public class Main {
 
 	public static void main(String[] args) {
-		switch (args.length) {
-		case 1: Application.loadProperties(args[0].endsWith(".properties") ? args[0] : args[0]+".properties");
-		case 0: /* intentional fall through from above */
-			Application app = new Application(DolphinFile.GetResultsPath());
-			SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
-					app.start();
-				}
-			});
-			break;
-		default:
-			System.err.println("Invalid arguments");
-		}
+		Application app = new Application(DolphinFile.GetResultsPath());
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				app.start();
+			}
+		});
 	}
 }
